4d2cb8efb588c9f24f47e2d7a5f6be3cb200764d
Nikolay Sisyukin <n@sisyukin.ru>
2024-12-27 22:02:13 +0300

    UPD

--- wg-analyze.py
+++ wg-analyze.py
@@ -36,7 +36,7 @@ def printTable(data):
     w_priv_ip = 16
     w_pub_ip = 16
     w_tr = 16
-    w_time = 45
+    w_time = 25
     separator_symbol = '-'
     separator_len = w_name + w_priv_ip + w_pub_ip + w_tr * 2 + w_time + 12
     seperator_line = ''.join([separator_symbol for i in range(separator_len)])
@@ -44,7 +44,9 @@ def printTable(data):
     print(f'{'NAME'.ljust(w_name)}| {'PRIVATE IP'.ljust(w_priv_ip)}| {'PUBLIC IP'.ljust(w_pub_ip)}| {'TX'.ljust(w_tr)} | {'RX'.ljust(w_tr)} | {'LATEST HANDSHAKE'.ljust(w_time)}')
     print(seperator_line)
     for val in data.values():
-        print(f'{val['name'].ljust(w_name)}| {val['private_ip'].ljust(w_priv_ip)}| {val['public_ip'].ljust(w_pub_ip)}| {val['TX'].rjust(w_tr)} | {val['RX'].rjust(w_tr)} | {val['latest_handshake'].rjust(w_time)}')
+        print(f'{val['name'].ljust(w_name)}| {val['private_ip'].ljust(w_priv_ip)}| {val['public_ip'].ljust(w_pub_ip)}| ', end='')
+        print(f'{val['TX'].rjust(w_tr)} | {val['RX'].rjust(w_tr)} | ', end='')
+        print(f'{val['latest_handshake'].rjust(w_time)}')
     print(seperator_line)
 
 # ----------------------------------------------------------------------
@@ -52,7 +54,7 @@ def printTable(data):
 def analyze(conf_filename='/etc/wireguard/wg0.conf', show_table=True, sort_table_key='private_ip', show_json=True):
     conf = subprocess.run(['grep', '-i', 'peer', '-A3', conf_filename], capture_output=True, text=True).stdout.splitlines()
     wg = subprocess.run(['wg'], capture_output=True, text=True).stdout.splitlines()
-    
+
     conf_json = {}
     for i, line in enumerate(conf):
         if 'peer' in line.lower():
@@ -66,10 +68,23 @@ def analyze(conf_filename='/etc/wireguard/wg0.conf', show_table=True, sort_table
     for i, line in enumerate(wg):
         if 'endpoint' in line.lower():
             id = wg[i+1].split()[2].replace(',', '')
-            
+
             conf_json[id]['public_ip'] = wg[i].split()[1].split(':')[0]
-            conf_json[id]['latest_handshake'] = ' '.join(wg[i+2].split()[2::])
-            
+            temp_handshake = []
+            for word in wg[i+2].split()[2::]:
+                if word.isdigit():
+                    temp_handshake.append(f'{word.zfill(2)}')
+
+                else:
+                    temp_handshake.append(word)
+            conf_json[id]['latest_handshake'] = ' '.join(temp_handshake)
+            dict_for_replace = {' seconds':'s', ' second':'s',
+                                ' minutes':'m', ' minute':'m',
+                                ' hours':'h', ' hour':'h',
+                                ' days':'d', ' day':'d'}
+            for word in dict_for_replace.items():
+                conf_json[id]['latest_handshake'] = conf_json[id]['latest_handshake'].replace(word[0], word[1])
+
             temp_rx = wg[i+3].split()[1:3:]
             temp_tx = wg[i+3].split()[4:6:]
             if len(temp_rx) > 1:
@@ -78,7 +93,7 @@ def analyze(conf_filename='/etc/wireguard/wg0.conf', show_table=True, sort_table
             else:
                 temp_rx[1] = 0.001
                 temp_tx[1] = 0.001
-            
+
             conf_json[id]['RX'] = f'{((float(temp_rx[0]) * float(temp_rx[1])) / MB):,.2f} MiB'.replace(',', ' ')
             conf_json[id]['TX'] = f'{((float(temp_tx[0]) * float(temp_tx[1])) / MB):,.2f} MiB'.replace(',', ' ')
 
